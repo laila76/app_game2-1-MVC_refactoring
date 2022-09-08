076fbe6d49a966ec7cd2f1860d362ff1b557c5a6
laila <laila.elmohannii@gmail.com>
2022-09-08 12:24:55 +0200

    show

--- models/database.php
+++ models/database.php
@@ -29,64 +29,10 @@ function getPDO(): PDO
     }
 }
 
-/**
- * this function return all games in array
- * 
- * @return array 
- */
-function getAllGames(): array
-{
-    $pdo = getPDO() ;
-    $sql = "SELECT * FROM jeux ORDER BY name";
-    $query = $pdo->prepare($sql);
-    $query->execute();
-    $games = $query->fetchAll();
 
-    return $games ;
-}
 
-/**
- * This function return current id of item
- * @return int
- */
-function getID(): int
-{
-    if (!empty($_GET['id']) && is_numeric($_GET['id'])) {
-        $id = clear_xss($_GET['id']);
-    } else {
-        $_SESSION["error"] = "URL invalide";
-        header("location: index.php");
-    }
-    return $id;
-}
 
-/**
- * This function return a single game
- * @return array 
- */
-function getGame(): array
-{
-    $pdo = getPDO() ;
-    $id = getID();
-    // 3- requette (query in english) vers BDD
-    $sql = "SELECT * FROM jeux WHERE id=:id";
-    // 4- préparation de la requette
-    $query = $pdo->prepare($sql);
-    // 5- securiser la requette contre injection sql
-    $query->bindValue(':id', $id, PDO::PARAM_INT);
-    // 6- executer la requette vers la BDD
-    $query->execute();
-    // 7- on stock tout ds une variable
-    $game = $query->fetch();
-    // debug_array($game);
-    // $game = [];
 
-    if (!$game) {
-        $_SESSION["error"] = "Ce jeu n'est pas disponible.";
-        header("location: index.php");
-    }
-    return $game ;
-}
 
 /**
  * This function delete an item
--- show.php
+++ show.php
@@ -3,7 +3,10 @@ session_start();
 // include PDO pour la connexion BDD
 require_once("models/database.php");
 // debug_array($_GET)
-$game= getGame();
+require_once("models/Game.php");
+$model = new Game();
+$game= $model->getGame();
 $title = $game['name'];
 require("view/showPage.php");
+
 ?>
